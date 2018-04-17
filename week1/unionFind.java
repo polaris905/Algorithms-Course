b08104419d55d00d9d5b462a73619860ee732dd0
POLARIS <37786972+polaris905@users.noreply.github.com>
2018-04-17 21:59:05 +0800

    Update and rename quickFind.java to week1/unionFind.java

--- week1/unionFind.java
+++ week1/unionFind.java
@@ -1,3 +1,4 @@
+//第一种算法
 public class QuickFindUF {
   private int[] id;
   
@@ -24,4 +25,33 @@ public class QuickFindUF {
       }
     }
   }
+}
+
+//第二种算法
+public class QuickUnionUF {
+	private int[] id;
+
+	public QuickUnionUF(int N) {
+		id = new int[N];
+		for (int i = 0; i < N; i++) {
+			id[i] = i;
+		}
+	}
+
+	private int root(int i) {
+		while(i != id[i]) {
+			i = id[i];
+		}
+		return i;
+	}
+
+	public boolean connected(int p, int q) {
+		return root(p) == root(q);
+	}
+
+	public void union(int p, int q) {
+		int i = root(p);
+		int j = root(q);
+		id[i] = j;
+	}
 }
