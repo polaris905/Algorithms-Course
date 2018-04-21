224e1a747b7a7005f18478593904bd379d70bc37
POLARIS <37786972+polaris905@users.noreply.github.com>
2018-04-21 23:43:34 +0800

    Update WeightedQuickUnionUF.java

--- week1/WeightedQuickUnionUF.java
+++ week1/WeightedQuickUnionUF.java
@@ -2,6 +2,7 @@
 这是Quick-Union的改进算法，在Quick-Union算法中，最糟糕的情况是N个site纵向排列成为一颗N个结点的树，这是因为在执行union时是嫁接顺序任意的
 本算法声明了一个数组sz，用来记录每个component的大小，在连接时总是选取sz值小的连接到sz值大的，如此，树的深度为lgN
 */
+// 这个还不是完整版
 
 public class WeightedQuickUnionUF {
   private int[] id; // parent link (site indexed)
