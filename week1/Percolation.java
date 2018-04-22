905a7b6e3e585e5fda4bce588af4ab243a40a763
POLARIS <37786972+polaris905@users.noreply.github.com>
2018-04-22 16:43:07 +0800

    Update Percolation.java

--- week1/Percolation.java
+++ week1/Percolation.java
@@ -1,88 +1,103 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-// 现在的问题是每次第一行都自动为open且full的状态，但是用main测试没发现问题
-
 public class Percolation {
-    private int sizes;
-    private boolean[] opened; 
+    private final int sizes;
+    private final WeightedQuickUnionUF uf;
+    private boolean[] isOpen;
     private int openSites;
-    WeightedQuickUnionUF uf;
+    private int topSite;
     
-    public Percolation(int n) {  // create n-by-n grid, with all sites blocked             
-       sizes = n;
-       opened = new boolean[n * n];
-       openSites = 0;
-       uf = new WeightedQuickUnionUF(n * n);     
-       for(int i = 1; i < n; i++) {
-           uf.union(0, i);
-       }      
-       for(int i = 0; i < n * n; i++) {
-           opened[i] = false;
+    // create n-by-n grid, with all sites blocked
+    public Percolation(int n) {
+       if (n <= 0)
+           throw new IllegalArgumentException("Illegal constructor Argument");
+       else {
+           sizes = n;
+           isOpen = new boolean[n * n + 1];
+           openSites = 0;
+           topSite = 0;
+           for (int i = 0; i <= n * n; i++) {
+               isOpen[i] = false;
+           }
+           uf = new WeightedQuickUnionUF(n * n + 1);
+           for (int i = 1; i <= n; i++) {
+               uf.union(topSite, i);
+           } 
        }
     }
-
+    
+    private boolean isValid(int row, int col) {
+        if ((row > 0 && row <= sizes) && (col > 0 && col <= sizes))
+            return true;
+        return false;
+    }
+    
     private int xyTo1D(int row, int col) {
-        return (row - 1) * sizes + col - 1;
+        return (row - 1) * sizes + col;
     }
     
     private void unionNeibourgh(int row, int col) {
-        int p = xyTo1D(row,col);
-        if(p % sizes == 0) {
-            if(isOpen(row, col + 1)) uf.union(p, xyTo1D(row, col + 1));
+        int p = xyTo1D(row, col);
+        if (p % sizes == 1) {
+            if (isOpen(row, col + 1)) uf.union(p, xyTo1D(row, col + 1));
         }
-        else if(p % sizes == (sizes - 1)) {
-            if(isOpen(row, col - 1)) uf.union(p, xyTo1D(row, col - 1));
-        }
-        else {
-            if(isOpen(row, col + 1)) uf.union(p, xyTo1D(row, col + 1));
-            if(isOpen(row, col - 1)) uf.union(p, xyTo1D(row, col - 1));
-        }
-        if(p < sizes) {
-            if(isOpen(row + 1, col)) uf.union(p, xyTo1D(row + 1, col));
-        }
-        else if(p >= sizes * (sizes - 1)) {
-            if(isOpen(row - 1, col)) uf.union(p, xyTo1D(row - 1, col));
+        else if (p % sizes == 0) {
+            if (isOpen(row, col - 1)) uf.union(p, xyTo1D(row, col - 1));
         }
         else {
-            if(isOpen(row + 1, col)) uf.union(p, xyTo1D(row + 1,col));
-            if(isOpen(row - 1, col)) uf.union(p, xyTo1D(row - 1,col));
-        }                                              
+            if (isOpen(row, col + 1)) uf.union(p, xyTo1D(row, col + 1));
+            if (isOpen(row, col - 1)) uf.union(p, xyTo1D(row, col - 1));
+        }
+        if (p <= sizes) {
+            if (isOpen(row + 1, col)) uf.union(p, xyTo1D(row + 1, col));
+        }
+        else if (p > sizes * (sizes - 1)) {
+            if (isOpen(row - 1, col)) uf.union(p, xyTo1D(row - 1, col));
+        }
+        else {
+            if (isOpen(row + 1, col)) uf.union(p, xyTo1D(row + 1, col));
+            if (isOpen(row - 1, col)) uf.union(p, xyTo1D(row - 1, col));
+        }
     }
     
-    public void open(int row, int col) {   // open site (row, col) if it is not open already
-        if (!opened[xyTo1D(row, col)]) {
-            opened[xyTo1D(row, col)] = true; 
-            openSites++;
-            unionNeibourgh(row, col);
+    // open site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (isValid(row, col)) {
+            if (!isOpen[xyTo1D(row, col)]) {
+                openSites++;
+                isOpen[xyTo1D(row, col)] = true;
+                if (sizes != 1) unionNeibourgh(row, col);
+            }
         }
+        else throw new IllegalArgumentException("Illegal Argument"); 
     }
     
     public boolean isOpen(int row, int col) { // is site (row, col) open?
-        return opened[xyTo1D(row, col)];
+        if (isValid(row, col)) return isOpen[xyTo1D(row, col)];
+        else throw new IllegalArgumentException("Illegal Argument");   
     }
     
     public boolean isFull(int row, int col) { // is site (row, col) full?
-        return uf.connected(xyTo1D(row, col), 0);
+        if (isValid(row, col)) {
+            if (isOpen(row, col))
+                return uf.connected(xyTo1D(row, col), 0);
+            else return false;
+        }
+        else throw new IllegalArgumentException("Illegal Argument"); 
     }
                      
-    public int numberOfOpenSites() {      // number of open sites
+    public int numberOfOpenSites() { // number of open sites
         return openSites;
     }
     
-    public boolean percolates() {             // does the system percolate?
-        for(int i = 1; i <= sizes; i++)
-        {
-            if(isFull(sizes, i)){
+    public boolean percolates() { // does the system percolate?
+        for (int i = 1; i <= sizes; i++) {
+            if (isFull(sizes, i)) 
                 return true;
-            }
         }
         return false;
     }
   
     public static void main(String[] args)  { // test client (optional)
-        Percolation per = new Percolation(5);
-        System.out.println(per.percolates());
     }
 }
